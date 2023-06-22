0e9d5b89a41899f0e1e6a84a140554743199e1db
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-06-22 17:41:31 +0200

    cp

---
+++ cpp01/ex05/src/main.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include "../Include/Harl.hpp"
+
+int main(void) 
+{
+	Harl log;
+	log.complain("debug");
+	return (0);
+}
