40963048bea5d3fa026c6076d15651bb1424eefe
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2023-11-13 14:50:50 +0100

    c07e3ok

--- cpp07/ex01/Include/iter.hpp
+++ cpp07/ex01/Include/iter.hpp
@@ -2,8 +2,8 @@
 
 
 template < typename T, typename U, typename V >
-void iter(T  list, U  lenght, V  fonction) {
-	for (int i = 0; i < lenght; i++) {
+void iter(T  list, U  length, V  fonction) {
+	for (int i = 0; i < length; i++) {
 		list[i] = fonction(list[i]);
 	}
 }
