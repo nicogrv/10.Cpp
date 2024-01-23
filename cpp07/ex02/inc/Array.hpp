480b134bc25e3c0bb5aa7e3ef6247b6efbbed0e9
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2024-01-23 15:38:56 +0100

    cpp07 e2

--- cpp07/ex02/inc/Array.hpp
+++ cpp07/ex02/inc/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 17:20:26 by ngriveau          #+#    #+#             */
-/*   Updated: 2024/01/22 17:53:54 by ngriveau         ###   ########.fr       */
+/*   Updated: 2024/01/23 15:38:09 by ngriveau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,35 +22,49 @@ class Array
 		Array()
 		{
 			this->lenght = 1;
-			this->tab = new T;
+			this->tab = new T[1]();
 		}
+
 		~Array()
 		{
-			std::cout << "DELETE" << std::endl;
-			// if (this->tab)
 			delete [] this->tab;
 		}
+
 		Array(Array &copy)
 		{
-			this->tab = new T[copy.lenght];
+			this->tab = new T[copy.lenght]();
 			this->lenght = copy.lenght;
+			for (unsigned int i = 0; i < copy.lenght; i++)
+				this->tab[i] = copy.tab[i];
 		}
+
 		Array &operator=(Array &copy)
 		{
 			delete [] this->tab;
 			this->lenght = copy.lenght;
-			std::cout << copy.lenght << std::endl;
+			this->tab = new T[this->lenght]();
 			for (unsigned int i = 0; i < copy.lenght; i++)
-				std::cout << "COPY" << std::endl;
+				this->tab[i] = copy.tab[i];
 			return (*this);
 		}
 
 		Array(unsigned int N)
 		{
 			this->lenght = N;
-			this->tab = new T[N];
+			this->tab = new T[N]();
 		}
-	
+
+		T& operator [](unsigned int idx)
+		{
+			if (0 <= idx && idx < this->lenght)
+				return this->tab[idx];
+			throw std::runtime_error("Out of range");
+		}
+
+
+		unsigned int size()
+		{	return (this->lenght);}
+
 	private:
 		T *tab;
 		unsigned int lenght;
--- cpp07/ex02/srcs/main.cpp
+++ cpp07/ex02/srcs/main.cpp
@@ -6,18 +6,141 @@
 /*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 17:20:03 by ngriveau          #+#    #+#             */
-/*   Updated: 2024/01/22 17:50:03 by ngriveau         ###   ########.fr       */
+/*   Updated: 2024/01/23 15:36:57 by ngriveau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Array.hpp"
 
+
+
+template <typename T>
+
+void displayArray(Array<T> array, Array<T> array2, std::string name1, std::string name2)
+{
+	std::cout << name1 <<": ";
+	for (unsigned int i = 0; i < array.size(); i++)
+		std::cout << i << ":" << array[i] << " ";
+	std::cout << std::endl;
+	std::cout << name2 << ": ";
+	for (unsigned int i = 0; i < array2.size(); i++)
+		std::cout << i << ":" << array2[i] << " ";
+	std::cout << std::endl << std::endl;
+}
+
 int main()
 {
-	Array<int> coucou(12);
-	Array<int> salut(coucou);	
+	std::cout << std::endl << "------------------Array_int(1)-------------------" << std::endl;
+	
+	try
+	{
+		Array<int> Array_int;	
+		std::cout << "Array_int size : " << Array_int.size() << std::endl;
+		std::cout << "Array_int[0]: " << Array_int[0] << std::endl;
+		std::cout << "Array_int[0] = 15"  << std::endl;
+		Array_int[0] = 15;
+		std::cout << "Array_int[0]: " << Array_int[0] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	std::cout << std::endl << "------------------Array_string(1)-------------------" << std::endl ;
 
-	Array<int> aurevoir(12);
-	Array<int> ciao(1);
-	ciao = aurevoir;
+	try
+	{
+		Array<std::string> Array_string ;
+		std::cout << "Array_string size : " << Array_string.size() << std::endl;
+		std::cout << "Array_string[0]: " << Array_string[0] << std::endl;
+		std::cout << "Array_string[0] = Bonjour les amis"  << std::endl;
+		Array_string[0] = "Bonjour les amis";
+		std::cout << "Array_string[0]: " << Array_string[0] << std::endl;
+	}	
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl << "------------------EgalArray-------------------" << std::endl ;
+
+
+
+	try
+	{
+		Array<std::string> array(5);
+		Array<std::string> egalArray(10);
+		std::cout << "array size    : " << array.size() << std::endl;
+		std::cout << "egalArray size: " << egalArray.size() << std::endl;
+		std::cout << std::endl;
+
+		array[0] = "|Bonjour|";
+		array[2] = "|Ca va ?|";
+		std::cout << "array[0] = |Bonjour| && array[2] = |Ca va ?|" << std::endl << std::endl;
+
+		displayArray(array, egalArray, "array", "egalArray");
+
+		egalArray = array;
+		std::cout << "egalArray = array" << std::endl << std::endl;
+		
+		displayArray(array, egalArray, "array", "egalArray");
+
+		std::cout << "array size    : " << array.size() << std::endl;
+		std::cout << "egalArray size: " << egalArray.size() << std::endl << std::endl;;
+		array[4] = "|Bien|";
+		std::cout << "array[4] = |Bien|" << std::endl << std::endl;
+
+		displayArray(array, egalArray, "array", "egalArray");
+
+		Array<std::string> copyArray(array);
+		
+		std::cout << std::endl << "------------------CopyArray-------------------" << std::endl ;
+
+		
+		displayArray(array, copyArray, "array", "copyArray");
+		
+		array[1] = "|Test|";
+		std::cout << "array[1] = |Test|" << std::endl << std::endl;
+		
+		displayArray(array, copyArray, "array", "copyArray");
+
+		std::cout << "array size    : " << array.size() << std::endl;
+		std::cout << "copyArray size: " << copyArray.size() << std::endl;
+		std::cout << std::endl;
+
+		
+	}	
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl << "------------------CopyArray-------------------" << std::endl ;
+
+	Array<std::string> array(10);
+	std::cout << "New array(10)" << std::endl;
+	try
+	{
+		std::cout << "try array[0]: ";
+		array[0];
+		std::cout << "OK" << std::endl;
+		std::cout << "try array[-1]: ";
+		array[-1];
+	}	
+	catch (std::exception &e)
+	{
+		std::cout << "Error exception: " << e.what() << std::endl << std::endl;
+	}
+	try
+	{
+		std::cout << "try array[9]: ";
+		array[9];
+		std::cout << "OK" << std::endl;
+		std::cout << "try array[10]: ";
+		array[10];
+	}	
+	catch (std::exception &e)
+	{
+		std::cout << "Error exception: " << e.what() << std::endl;
+	}
 }
