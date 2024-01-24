63ae7fb9940ef4423980bb057d44e51631265988
Nicolas GRIVEAU <nicolasgri45@icloud.com>
2024-01-24 13:55:28 +0100

    cpp08 e0

---
+++ cpp08/ex00/inc/easyfind.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 10:50:01 by ngriveau          #+#    #+#             */
+/*   Updated: 2024/01/24 13:54:08 by ngriveau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+# include <iterator> 
+# include <vector>
+# include <list>
+# include <deque>
+# include <iostream>
+
+template <typename T>
+
+unsigned int easyfind(T find, int number)
+{
+	unsigned int			i;
+	typename T::iterator	it;
+
+	i = 0;
+	for (it = find.begin(); it != find.end(); it++)
+	{
+		i++;
+		if (*it == number)
+			return (i);
+	}
+	throw std::runtime_error("no occurrence");
+}
+
+#endif
---
+++ cpp08/ex00/srcs/main.cpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 16:26:29 by ngriveau          #+#    #+#             */
+/*   Updated: 2024/01/24 13:55:06 by ngriveau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/easyfind.hpp"
+
+int main( void )
+{
+	std::vector<int> vec;
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	vec.push_back(5);
+	vec.push_back(6);
+	try
+	{
+		int index = easyfind(vec, 10);
+		std::cout << "OK index: " << index << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+	std::cout << "----------------------------------------" << std::endl;
+
+	std::deque<int> deque;
+	deque.push_back(10);
+	deque.push_back(20);
+	deque.push_back(30);
+	deque.push_back(40);
+	deque.push_back(50);
+	deque.push_back(60);
+	deque.push_back(100);
+
+	try
+	{
+		int index = easyfind(deque, 40);
+		std::cout << "OK index: " << index << std::endl;
+
+		index = easyfind(deque, 110);
+		std::cout << "OK index: " << index << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << "----------------------------------------" << std::endl;
+
+ 	std::list<int> list;
+
+    list.push_back(42);
+    list.push_back(123);
+    list.push_back(1254);
+
+	try
+	{
+		int index = easyfind(list, 123);
+		std::cout << "OK index: " << index << std::endl;
+
+		index = easyfind(list, 1212);
+		std::cout << "OK index: " << index << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	return 0;
+}
